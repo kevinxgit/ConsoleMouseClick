02ac99cbf468fcf61a0ba9d143c5256a2a04aa22
Kevin Xiong <Kevin.Xiong@Fiserv.com>
2020-08-19 02:31:21 -0500

    test

---
+++ DetectMouse.cs
@@ -0,0 +1,28 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMouseClick
+{
+    public class DetectMouseClick
+    {
+        public static void ListenForMouseEvents()
+        {
+            Console.WriteLine("Listening to mouse clicks.");
+
+            //When a mouse button is pressed 
+            Hook.GlobalEvents().MouseDown += async (sender, e) =>
+            {
+                Console.WriteLine($"Mouse {e.Button} Down at x:{e.X} y:{e.Y}");
+            };
+            //When a double click is made
+            Hook.GlobalEvents().MouseDoubleClick += async (sender, e) =>
+            {
+                Console.WriteLine($"Mouse {e.Button} button double clicked at x:{e.X} y:{e.Y}");
+            };
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleMouseClick
+{
+    class Program
+    {
+        private IKeyboardMouseEvents m_GlobalHook;
+
+        [MTAThread]
+        static void Main(string[] args)
+        {
+            //    var p = new Program();
+            //    p.Subscribe();
+
+            DetectMouseClick.ListenForMouseEvents();
+
+            Thread.CurrentThread.Priority = ThreadPriority.Highest;
+
+            //Run the app as a windows form application
+            Application.Run(new ApplicationContext());
+        }
+
+    }
+}
